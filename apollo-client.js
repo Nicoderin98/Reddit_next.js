6461609a8f2cc0a4003c1cb94bb1c08dabebe42f
Nicolo De Rinaldis <nicoderin98@gmail.com>
2022-06-01 20:31:13 +0200

    second-push

---
+++ apollo-client.js
@@ -0,0 +1,10 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "https://torgau.stepzen.net/api/singed-walrus/__graphql",
+    headers: {
+      Authorization: 'Apikey ${process.env-NEXT_PUBLIC_STEPZEN_KEY}'
+    },
+    cache: new InMemoryCache(),
+});
+export default client;
